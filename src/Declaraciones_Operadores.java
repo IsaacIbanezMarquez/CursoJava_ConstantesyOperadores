d1cdb11f446cf8a8e0fb242ab1ed22008e71a077
Isaac Ibanez Marquez <isaacibanez00@gmail.com>
2022-01-08 19:53:41 +0100

    Initial commit

---
+++ src/Declaraciones_Operadores.java
@@ -0,0 +1,37 @@
+
+
+    // CURSO JAVA.
+
+    // DECLARACIÓN DE VARIABLES:
+
+
+public class Declaraciones_Operadores
+{
+    public static void main(String[] args)
+    {
+        // Iniciar variable con valor.
+        int a = 5;
+
+        // Iniciar variable sin valor.
+        int b;
+
+        // Instanciar valor de variable declarada:
+        b = 7;
+
+        // Variable con valor de la suma de otras variables.
+        int c = b + a;
+
+        // Variable++ , incrementa en 1 al valor de la variable.
+        c++;
+
+        // Variable+= , incrtementa en X el valor de la variable:
+
+        c+=6;
+        c-=6;
+
+        // Comportamiento IntelliSense, System.out.println()[ imprimir en consola]:
+
+        System.out.println(c);
+
+    }
+}
